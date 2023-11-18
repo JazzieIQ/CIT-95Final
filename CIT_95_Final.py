d32c8eff13963b727bee236e50ce665c63c28641
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-11-18 15:02:46 -0800

    update

--- CIT_95_Final.py
+++ CIT_95_Final.py
@@ -5,3 +5,33 @@
 # Date: 10-30-2023
 #
 # Programmer's name: Matthew Gutierrez
+import pygame
+def main ():
+    # Program Intro
+    print("\n** Welcome to Mac Mac's Program for creating a 3D environment. **\n")
+    def start():
+        start_game = input("Would you like to see play the game. Y/N: ", ).lower()
+        if start_game == "y":
+            resolution = (400,300)
+            white = (255,255,255)
+            black = (0,0,0)
+            red = (255,0,0)
+            screen = pygame.display.set_mode(resolution)
+            while True:
+                screen.fill(white)
+                pygame.display.flip()
+                for event in pygame.event.get():
+                    if event.type == pygame.locals.QUIT:
+                        print("\nThank you for testing my 3D environment.\nProgram terminated.")
+                        pygame.quit()
+                        exit(1)
+        else:
+            print("\nThis program is designed to render a three dimensional environment.\n")
+            return_to_start = input("If you don't want to see my hard work then what am I good for; do you want to see it? Y/N: \n")
+            if return_to_start[0] == "y":
+               start()
+            else:
+                 print("\nK Bye.")
+                 exit(1)
+    start()
+main()
