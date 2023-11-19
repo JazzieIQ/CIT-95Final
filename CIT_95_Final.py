af60d47f06dbec5feb04097abf4d199275d5d7ec
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-11-18 17:40:46 -0800

    update

--- CIT_95_Final.py
+++ CIT_95_Final.py
@@ -6,32 +6,38 @@
 #
 # Programmer's name: Matthew Gutierrez
 import pygame
-def main ():
+
+
+def main():
     # Program Intro
     print("\n** Welcome to Mac Mac's Program for creating a 3D environment. **\n")
+
     def start():
         start_game = input("Would you like to see play the game. Y/N: ", ).lower()
         if start_game == "y":
-            resolution = (400,300)
-            white = (255,255,255)
-            black = (0,0,0)
-            red = (255,0,0)
+            resolution = (1200, 900)
+            dungeon_lily = pygame.image.load("Lily's Dungeon Floor.png")
+            white = (255, 255, 255)
+            black = (0, 0, 0)
+            red = (255, 0, 0)
             screen = pygame.display.set_mode(resolution)
             while True:
-                screen.fill(white)
+                #screen.fill(white)
+                screen.blit(dungeon_lily, (0, 0))
                 pygame.display.flip()
                 for event in pygame.event.get():
-                    if event.type == pygame.locals.QUIT:
+                    if event.type == pygame.QUIT:
                         print("\nThank you for testing my 3D environment.\nProgram terminated.")
                         pygame.quit()
                         exit(1)
         else:
             print("\nThis program is designed to render a three dimensional environment.\n")
-            return_to_start = input("If you don't want to see my hard work then what am I good for; do you want to see it? Y/N: \n")
+            return_to_start = input(
+                "If you don't want to see my hard work then what am I good for; do you want to see it? Y/N: \n")
             if return_to_start[0] == "y":
-               start()
+                start()
             else:
-                 print("\nK Bye.")
-                 exit(1)
+                print("\nK Bye.")
+                exit(1)
     start()
 main()
